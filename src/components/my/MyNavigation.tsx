0e4f45faddc7c14ef476b6a5b3a8a9b90fd7156e
Gahee Kim <soprue@naver.com>
2024-04-20 16:31:07 +0900

    Feat: MyPageLayout 구현

---
+++ src/components/my/MyNavigation.tsx
@@ -0,0 +1,68 @@
+import { NavLink } from 'react-router-dom';
+import useUser from '@hooks/useUser';
+
+function MyNavigation() {
+  const user = useUser();
+
+  return (
+    <div className="w-[200px] border-r border-gray-200 min-h-[calc(100vh-64px)] flex flex-col p-4 pt-10">
+      <nav className="flex flex-col text-xl gap-5">
+        <NavLink
+          to="/my"
+          end
+          className={({ isActive }) =>
+            isActive ? 'text-black' : 'text-gray-500'
+          }
+        >
+          프로필
+        </NavLink>
+        {user?.userType === 'organizer' && (
+          <>
+            <NavLink
+              to="/my/events"
+              end
+              className={({ isActive }) =>
+                isActive ? 'text-black' : 'text-gray-500'
+              }
+            >
+              이벤트 관리
+            </NavLink>
+            <NavLink
+              to="/my/orders"
+              end
+              className={({ isActive }) =>
+                isActive ? 'text-black' : 'text-gray-500'
+              }
+            >
+              주문 관리
+            </NavLink>
+          </>
+        )}
+        {user?.userType === 'buyer' && (
+          <>
+            <NavLink
+              to="/my/tickets"
+              end
+              className={({ isActive }) =>
+                isActive ? 'text-black' : 'text-gray-500'
+              }
+            >
+              이벤트 참여 내역
+            </NavLink>
+            <NavLink
+              to="/my/likes"
+              end
+              className={({ isActive }) =>
+                isActive ? 'text-black' : 'text-gray-500'
+              }
+            >
+              찜한 이벤트
+            </NavLink>
+          </>
+        )}
+      </nav>
+    </div>
+  );
+}
+
+export default MyNavigation;
--- src/pages/layout/MyPageLayout.tsx
+++ src/pages/layout/MyPageLayout.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from 'react-router-dom';
 
 import Header from '@components/layout/Header';
+import MyNavigation from '@components/my/MyNavigation';
 
 function MyPageLayout() {
   return (
     <div className="min-h-dvh relative overflow-hidden font-pretendard">
       <Header />
-      <div className="container">
-        <Outlet />
+
+      <div className="container flex">
+        <MyNavigation />
+        <div className="w-[calc(100%-200px)] pl-10 pt-10 min-h-[calc(100dvh-64px)]">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
