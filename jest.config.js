5d0cafd32883384e2b6772cc6a3319263d368cd7
Gahee Kim <soprue@naver.com>
2024-05-13 12:56:24 +0900

    Chore: jest.config.js 수정

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,10 @@ module.exports = {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
   },
+  testMatch: [
+    '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
