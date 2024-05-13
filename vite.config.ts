b0dce71b89b2898bc8d47794b7ad79968a0c99c4
Gahee Kim <soprue@naver.com>
2024-05-13 15:15:46 +0900

    Chore: rollup-plugin-visualizer 기본 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
@@ -7,7 +8,10 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true, filename: 'bundle-analysis.html' }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
