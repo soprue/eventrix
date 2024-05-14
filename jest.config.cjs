3d0a20f696209d18a8674b511d2d40f1df7eb019
Gahee Kim <soprue@naver.com>
2024-05-14 23:07:30 +0900

    Remove: 테스트 폴더명 변경

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,20 +5,48 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png|webp)$': '<rootDir>/__mocks__/fileMock.js',
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@shared/(.*)$': '<rootDir>/src/components/shared/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
     '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
   },
   testMatch: [
     '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
     '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
   ],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: {
+                  env: {
+                    // Replicate as .env.local
+                    VITE_API_KEY: 'asdfasdf',
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(firebase|@firebase))'],
   fakeTimers: {
     enableGlobally: true,
   },
--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -3,24 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
 import { getStorage } from 'firebase/storage';
 
-const {
-  VITE_API_KEY,
-  VITE_AUTH_DOMAIN,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BUCKET,
-  VITE_MESSAGING_SENDER_ID,
-  VITE_APP_ID,
-  VITE_MEASUREMENT_ID,
-} = import.meta.env;
-
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGING_SENDER_ID,
-  appId: VITE_APP_ID,
-  measurementId: VITE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
